8c1551a8888d7f6e8df61807b7ff60756b229c23
unknown <imperialjapan869@gmail.com>
2025-03-25 16:34:53 +0530

    Fix API endpoint URL for published courses

--- src/features/api/courseApi.js
+++ src/features/api/courseApi.js
@@ -74,7 +74,7 @@ export const courseApi = createApi({
     }),
     getPublishedCourse: builder.query({
       query: () => ({
-        url: `${COURSE_API}/published-courses`,
+        url: `/api/v1/published-courses`,
         method: "GET",
       }),
       providesTags: ["Published_Courses"],
